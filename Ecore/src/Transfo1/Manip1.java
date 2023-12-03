b46d4cf4aa84b26de517323c6546fb85f7bb0c83
zakaria <zakaria.hachm20@gmail.com>
2023-12-03 23:16:21 +0100

    fix: Fixing the changeable bug in the Transformation rules

--- Ecore/src/Transfo1/Manip1.java
+++ Ecore/src/Transfo1/Manip1.java
@@ -1,6 +1,8 @@
 package Transfo1;
 
 import java.io.IOException;
+import org.eclipse.emf.common.util.EList;
+
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EAttribute;
@@ -44,19 +46,89 @@ public class Manip1 {
         EObject outputModelRoot = MMSePackage.getEFactoryInstance().create(outputRootClass);
        
 
+        // Transformation rules
+        
+        // Projet.name to Config.name
+        setFeatureValue(outputModelRoot, MMSePackage, "config", "name", MRacine, MMePackage, "Projet", "name");
+
+    
+     // Projet.branch to Cloning.branch
+        EClass cloningClass = (EClass) MMSePackage.getEClassifier("Cloning");
+        EObject cloningObject = MMSePackage.getEFactoryInstance().create(cloningClass);
+        EAttribute branchFeature = (EAttribute) cloningClass.getEStructuralFeature("branch");
+        cloningObject.eSet(branchFeature, MRacine.eGet(((EClass) MMePackage.getEClassifier("Projet")).getEStructuralFeature("branch")));
+        ((EList<EObject>) outputModelRoot.eGet(((EClass) MMSePackage.getEClassifier("config")).getEStructuralFeature("stages"))).add(cloningObject);
+
+     // Projet.url to Cloning.url
+        EClass cloningClassUrl = (EClass) MMSePackage.getEClassifier("Cloning");
+        EObject cloningObjectUrl = MMSePackage.getEFactoryInstance().create(cloningClassUrl);
+        EAttribute urlFeature = (EAttribute) cloningClassUrl.getEStructuralFeature("url");
+        cloningObjectUrl.eSet(urlFeature, MRacine.eGet(((EClass) MMePackage.getEClassifier("Projet")).getEStructuralFeature("url")));
+        ((EList<EObject>) outputModelRoot.eGet(((EClass) MMSePackage.getEClassifier("config")).getEStructuralFeature("stages"))).add(cloningObjectUrl);
+
+        // Testing.cmdtest to Tests.shell
+
+
+
+        // Default values
+
+        // Default value for Cloning.credentialID
+        EClass cloningClassCredential = (EClass) MMSePackage.getEClassifier("Cloning");
+        EObject cloningObjectCredential = MMSePackage.getEFactoryInstance().create(cloningClassCredential);
+        EAttribute credentialIDFeature = (EAttribute) cloningClassCredential.getEStructuralFeature("credentialID");
+        cloningObjectCredential.eSet(credentialIDFeature, "personal-cloning-key");
+        ((EList<EObject>) outputModelRoot.eGet(((EClass) MMSePackage.getEClassifier("config")).getEStructuralFeature("stages"))).add(cloningObjectCredential);
+
+        // Default value for Build.cmd
+        EClass buildClassCmd = (EClass) MMSePackage.getEClassifier("Build");
+        EObject buildObjectCmd = MMSePackage.getEFactoryInstance().create(buildClassCmd);
+        EAttribute cmdFeature = (EAttribute) buildClassCmd.getEStructuralFeature("cmd");
+        buildObjectCmd.eSet(cmdFeature, "sh \"docker build -t ${dockerImageTag} .\"");
+        ((EList<EObject>) outputModelRoot.eGet(((EClass) MMSePackage.getEClassifier("config")).getEStructuralFeature("stages"))).add(buildObjectCmd);
+
+        // Default value for Deploy.cmd
+        EClass deployClassCmd = (EClass) MMSePackage.getEClassifier("Deploy");
+        EObject deployObjectCmd = MMSePackage.getEFactoryInstance().create(deployClassCmd);
+        EAttribute cmdDeployFeature = (EAttribute) deployClassCmd.getEStructuralFeature("cmd");
+        deployObjectCmd.eSet(cmdDeployFeature, "sh \"docker push ${dockerImageTag}\"");
+        ((EList<EObject>) outputModelRoot.eGet(((EClass) MMSePackage.getEClassifier("config")).getEStructuralFeature("stages"))).add(deployObjectCmd);
+
+
+        // Save Output Model:
+        Resource outputModelResource = resourceSet.createResource(URI.createFileURI("C:/Users/zakar/eclipse-workspace/Ecore/model/Output.model"));
+        outputModelResource.getContents().add(outputModelRoot);
+        try {
+            outputModelResource.save(null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         printEObject(outputModelRoot,0);
     }
 
     private static void setFeatureValue(EObject targetObject, EPackage targetPackage, String targetClassName, String targetFeatureName,
             EObject sourceObject, EPackage sourcePackage, String sourceClassName, String sourceFeatureName) {
         EClass targetClass = (EClass) targetPackage.getEClassifier(targetClassName);
-        EAttribute targetFeature = (EAttribute) targetClass.getEStructuralFeature(targetFeatureName);
+        EStructuralFeature targetFeature = targetClass.getEStructuralFeature(targetFeatureName);
 
         EClass sourceClass = (EClass) sourcePackage.getEClassifier(sourceClassName);
-        EAttribute sourceFeature = (EAttribute) sourceClass.getEStructuralFeature(sourceFeatureName);
+        EStructuralFeature sourceFeature = sourceClass.getEStructuralFeature(sourceFeatureName);
 
-        targetObject.eSet(targetFeature, sourceObject.eGet(sourceFeature));
+        if (targetFeature.isChangeable()) {
+            // Check if sourceFeature is an attribute
+            if (sourceFeature instanceof EAttribute) {
+                targetObject.eSet(targetFeature, sourceObject.eGet(sourceFeature));
+            } else {
+                // If sourceFeature is a reference, copy its value to the target feature
+                EObject referencedObject = (EObject) sourceObject.eGet(sourceFeature);
+                if (referencedObject != null) {
+                    targetObject.eSet(targetFeature, referencedObject);
+                }
+            }
+        } else {
+            System.err.println("Cannot set the value for non-changeable feature: " + targetFeatureName);
+        }
     }
+
 
     private static void setFeatureValue(EObject targetObject, EPackage targetPackage, String targetClassName, String targetFeatureName, Object value) {
         EClass targetClass = (EClass) targetPackage.getEClassifier(targetClassName);
@@ -65,7 +137,7 @@ public class Manip1 {
         targetObject.eSet(targetFeature, value);
     }
 
-  
+     //printEObject(outputModelRoot, 0);
     
 
     private static void printEObject(EObject eObject, int indent) {
