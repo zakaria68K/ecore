2f7658e7424f3ee567de2f7de4683c157d1b39bc
zakaria <zakaria.hachm20@gmail.com>
2023-12-18 10:25:40 +0100

    feat : loading the needed infos to prepare the transformations

---
+++ Ecore/src/Transfo2/Manip2.java
@@ -0,0 +1,47 @@
+package Transfo2;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+
+public class Manip2 {
+
+    public static void main(String[] args) {
+
+
+
+        // Register the XMI resource factory for the EMF resource set
+        Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("*", new XMIResourceFactoryImpl());
+
+        // Create an EMF resource set
+        ResourceSet resourceSet = new ResourceSetImpl();
+
+        // Load the input metamodel (InitConfig.ecore)
+        Resource MMResource = resourceSet.getResource(URI.createFileURI("C:/Users/zakar/eclipse-workspace/Ecore/model/InitConfig/InitConfig.ecore"), true);
+        EObject MMracine = MMResource.getContents().get(0);
+        EPackage MMePackage = (EPackage) MMracine;
+
+        // Load the output metamodel DockerMetamodel.ecore)
+        Resource MMSResource = resourceSet.getResource(URI.createFileURI("C:/Users/zakar/eclipse-workspace/Ecore/model/Docker/DockerMetamodel.ecore"), true);
+        EObject MMSracine = MMSResource.getContents().get(0);
+        EPackage MMSePackage = (EPackage) MMSracine;
+
+        // Load the input model (InitConfig.model)
+        String nsUri = MMePackage.getNsURI();
+        resourceSet.getPackageRegistry().put(nsUri, MMePackage);
+        Resource load_resource = resourceSet.getResource(URI.createFileURI("C:/Users/zakar/eclipse-workspace/Ecore/model/InitConfig/InitConfig.model"), true);
+        Resource MResource = load_resource;
+        EObject MRacine = MResource.getContents().get(0);
+
+        // Create an instance of the root class from the output metamodele
+        EClass outputRootClass = (EClass) MMSePackage.getEClassifier("config");
+        EObject outputModelRoot = MMSePackage.getEFactoryInstance().create(outputRootClass);
+    	
+    }
+	
+}
